c638a0e112f3498d128f05581122873c0a4ea616
Nick A <areekadann@mymacewan.ca>
2022-11-06 00:45:24 -0600

    feature: added custom user model and updated environ version

--- desk_tech/urls.py
+++ desk_tech/urls.py
@@ -8,3 +8,7 @@ urlpatterns = [
     path('confidential/', admin.site.urls),
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+admin.site.site_header = "DeskTech Admin"
+admin.site.site_title = "DeskTech Admin Portal"
+admin.site.index_title = "Welcome to the DeskTech Portal"
