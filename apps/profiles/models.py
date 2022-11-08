192320e52b0dbe804312d88462cbc2b0626798db
Nick A <areekadann@mymacewan.ca>
2022-11-07 21:05:34 -0700

    features: register, login, update, get user profiles

--- apps/profiles/models.py
+++ apps/profiles/models.py
@@ -14,21 +14,37 @@ class Gender(models.TextChoices):
 
 class Profile(TimeStampedUUIDModel): #dont really need a profile page.
    user = models.OneToOneField(User, related_name="profile", 
-   on_delete=models.CASCADE)
-   phone_number = PhoneNumberField(verbose_name=_("Phone Number"), max_length=30, default="+17804545454")
-   about_me = models.TextField(verbose_name=_("About Me"), default="say something about yourself")
+   on_delete=models.CASCADE
+   )
+   phone_number = PhoneNumberField(verbose_name=_("Phone Number"), max_length=30, default="+17804545454"
+   )
+   license = models.CharField(verbose_name=_("Real Estate license"), max_length=20, blank=True, null=True
+   )
+   about_me = models.TextField(verbose_name=_("About Me"), default="say something about yourself"
+   )
    profile_photo= models.ImageField(verbose_name=_("Profile Photo"), 
-   default="/profile_default.png")
+   default="/profile_default.png"
+   )
    gender= models.CharField(verbose_name=_("Gender"), choices=Gender.choices,
-   default=Gender.OTHER, max_length=20)
-   country= CountryField(verbose_name=_("Country"), default="CA", blank=False, null=False)
-   city=models.CharField(verbose_name=_("City"), max_length=180, default="Edmonton", blank=False, null=False)
+   default=Gender.OTHER, max_length=20
+   )
+   country= CountryField(verbose_name=_("Country"), default="CA", blank=False, null=False
+   )
+   city=models.CharField(verbose_name=_("City"), max_length=180, default="Edmonton", blank=False, null=False
+   )
    is_buyer= models.BooleanField(verbose_name=_("Buyer"), default=False,
-   help_text=_("Are you looking to become a member"))
+   help_text=_("Are you looking to become a member")
+   )
    is_seller= models.BooleanField(verbose_name=_("Seller"), default=False,
-   help_text=_("Are you looking to sell"))
-   top_seller= models.BooleanField(verbose_name=_("Top Seller"), default=False)
-   rating= models.DecimalField(max_digits=4, decimal_places=2, null=True, blank=True)
+   help_text=_("Are you looking to sell")
+   )
+   is_agent = models.BooleanField(verbose_name=_("Agent"), default=False,
+   help_text=_("Are you an agent?")
+   )
+   top_agent= models.BooleanField(verbose_name=_("Top Agent"), default=False
+   )
+   rating= models.DecimalField(max_digits=4, decimal_places=2, null=True, blank=True
+   )
    num_reviews = models.IntegerField(
     verbose_name=_("Number of Reviews"), default=0, null=True, blank=True
    )
--- apps/ratings/serializers.py
+++ apps/ratings/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Rating
 
-class RatingSerializer(serializers.ModelSerializers):
+class RatingSerializer(serializers.ModelSerializer):
     rater= serializers.SerializerMethodField(read_only=True)
     agent= serializers.SerializerMethodField(read_only=True)
 
